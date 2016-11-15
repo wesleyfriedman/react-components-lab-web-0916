e578efe2be18f532b5f29a5a17086b2ff0ff346a
wesleyfriedman <friedman.wesley@gmail.com>
2016-11-15 16:53:34 -0500

    Done

--- index.js
+++ index.js
@@ -5,11 +5,54 @@ const BUTCHER_PRODUCTS = [
   'Ribeye'
 ];
 
+const OlderCoaster = React.createClass ({
+  render() {
+    return React.createElement('div', {className: "oldercoaster"},[
+      React.createElement('p', {}, 'Two grannies having the time of their life!'),
+      React.createElement('p', {}, 'Passengers:'),
+      React.createElement('ul', {}, [
+        React.createElement('li', {}, 'Agnes'),
+        React.createElement('li', {}, 'Muriel')
+      ])
+    ]);
+  }
+})
+
+class InFrontOfYou extends React.Component {
+  render() {
+    // var stuff = '<strong>right in front of you</strong>'
+    return React.createElement('div', {},
+      [
+        React.createElement('p', {}, 'You shouldn\'t look too far.'),
+        [
+          React.createElement('p', {}, `Sometimes, the solution is `,
+          React.createElement('strong', {}, 'right in front of you.'))
+        ]
+      ]
+    )
+  }
+}
+
+class ButcherShop extends React.Component {
+  render() {
+    return React.createElement('div', {className: "butcher-shop"}, [
+    React.createElement('p', {}, 'Hello! We have the following products for sale today:'),
+    React.createElement('ul', {}, [
+      React.createElement('li', {}, 'Tenderloin'),
+      React.createElement('li', {}, 'Short ribs'),
+      React.createElement('li', {}, 'Beef shin'),
+      React.createElement('li', {}, 'Ribeye')
+    ])
+  ])
+  }
+}
+
+
 ReactDOM.render(
   React.createElement('div', {}, [
     React.createElement(OlderCoaster),
     React.createElement(InFrontOfYou),
-    React.createElement(ButcherShop)
+    // React.createElement(ButcherShop)
   ]),
   document.getElementById('main')
 );
